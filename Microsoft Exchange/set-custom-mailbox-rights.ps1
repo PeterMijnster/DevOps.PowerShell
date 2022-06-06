0a6027d00a897a2b7b03e173ff48b193ad89b457
Peter Mijnster <peter.mijnster@onetribe.nl>
2022-06-06 16:46:43 +0200

    commit

--- Microsoft Exchange/set-custom-mailbox-rights.ps1
+++ Microsoft Exchange/set-custom-mailbox-rights.ps1
@@ -1,10 +1,12 @@
-#Custom Variables
-$Mailbox = ""
-$MailboxAutomap = $false
-$MailboxPermission = "" #FullAccess, ChangePermission, ReadPermission
-$MailboxRecipientPermission "" #SendAs
-$UserFilter = ""
-$UserGroup = ""
+#Custom Mailbox Variables
+$Mailbox = "" #User the 'DisplayName' value of the mailbox
+$MailboxAutomap = $false #Allowed values: $true, $false
+$MailboxPermission = "" #Allowed values: FullAccess, ChangePermission, ReadPermission
+$MailboxRecipientPermission "" #Allowed values: SendAs
+
+#Custom User Variables (Choose One)
+$UserFilter = "" #Exmaple: DisplayName eq 'Peter Mijnster' or startswith(JobTitle,'IT')
+$UserGroup = "" #Use the 'DisplayName' value of the group
 
 #Set Variables
 if ($null -ne $UserFilter) {$Users = Get-AzureADUser -Filter $UserFilter}
