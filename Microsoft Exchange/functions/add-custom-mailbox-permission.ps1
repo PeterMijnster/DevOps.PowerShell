2c6164a8beb847e939276f43a51bcb8e411405c4
Peter Mijnster <peter.mijnster@onetribe.nl>
2022-06-06 17:58:24 +0200

    commit

---
+++ Microsoft Exchange/functions/add-custom-mailbox-permission.ps1
@@ -0,0 +1,31 @@
+function Add-CustomMailboxPermissionUser {
+    Param (
+        $Mailbox,
+        $MailboxAutomap,
+        $MailboxPermission,
+        $MailboxRecipientPermission,
+        $GroupDisplayName
+        $UserDisplayName
+    )
+
+    #Custom User Variables
+    if ($null -ne $GroupDisplayName) {$Users = Get-AzureADGroup -SearchString $GroupDisplayName| Get-AzureADGroupMember}
+    if ($null -ne $UserDisplayName) {$Users = Get-AzureADUser -SearchString $UserDisplayName}
+
+    #Add Mailbox Permissions
+    if ($null -ne $MailboxPermission) {
+        foreach $User in $Users {
+            Add-MailboxPermission -Identity $Mailbox -User $User.UserPrincipalName -AccessRights $MailboxPermission -InheritanceType All -AutoMapping $MailboxAutomap
+        }
+    }
+    #Add Mailbox Recipient Permissions
+    if ($null -ne $MailboxRecipientPermission) {
+        foreach $User in $Users {
+            Add-RecipientPermission -Identity $Mailbox -Trustee $User.UserPrincipalName -AccessRights $MailboxRecipientPermission -Confirm:$false
+        }
+    }
+}
+
+#Examples
+Add-CustomMailboxPermissionUser -Mailbox "OneTribe - DevOps" -MailboxAutomap $false -MailboxPermission "FullAccess" -MailboxRecipientPermission "SendAs" -GroupDisplayName "OneTribe - DevOps"
+Add-CustomMailboxPermissionUser -Mailbox "OneTribe - DevOps" -MailboxAutomap $false -MailboxPermission "FullAccess" -MailboxRecipientPermission "SendAs" -UserDisplayName "Peter Mijnster"
