c6e5d41f6d5c92fd40a2b77eaa153575b5605ff2
Peter Mijnster <peter.mijnster@onetribe.nl>
2022-06-06 15:34:23 +0200

    commit

---
+++ Microsoft Windows/create-shortcut-with-new-acl.ps1
@@ -0,0 +1,34 @@
+#Apply Global Variables
+$CommonDesktop = (New-Object -ComObject Shell.Application).NameSpace('shell:Common Desktop').Self.Path
+
+#Shortcut Custom Variables
+$Application = ""
+$ApplicationArguments = ""
+$ShortcutName = ""
+
+#Shortcust Custom ACL Variables
+$Identity = "" #Everyone
+$FileSystemRights = "" #FullControl
+
+#Create Shortcut and Set New Shortcut ACL
+if (!(Test-Path "$CommonDesktop\Shortcut.lnk")) {
+    #Create Shortcut
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $ShortcutTarget = "$Application"
+    $ShortcutTargetArguments = "$ApplicationArguments"
+    $ShortcutTargetPath = "$CommonDesktop\$ShortcutName.lnk"
+    $Shortcut = $WScriptShell.CreateShortcut($ShortcutTargetPath)
+    $Shortcut.Arguments = $ShortcutTargetArguments
+    $Shortcut.TargetPath = $ShortcutTarget
+    $Shortcut.Save()
+    
+    #Set New Shotcut ACL
+    $NewAcl = Get-Acl "$CommonDesktop\$ShortcutName.lnk"
+    $NewAclIdentity = $Identity
+    $NewAclFileSystemRights = $FileSystemRights
+    $NewAclType = "Allow"
+    $FileSystemAccessRuleArgumentList = $NewAclIdentity, $NewAclFileSystemRights, $NewAclType
+    $FileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $FileSystemAccessRuleArgumentList
+    $NewAcl.SetAccessRule($FileSystemAccessRule)
+    Set-Acl "$CommonDesktop\$ShortcutName.lnk" -AclObject $NewAcl
+    }
