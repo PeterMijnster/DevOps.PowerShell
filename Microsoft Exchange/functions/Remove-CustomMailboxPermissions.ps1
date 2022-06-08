f3d14c227707d2623df339ec79bf307a66251014
Peter Mijnster <peter.mijnster@soundofdata.nl>
2022-06-09 00:42:46 +0200

    commit

--- Microsoft Exchange/functions/Remove-CustomMailboxPermissions.ps1
+++ Microsoft Exchange/functions/Remove-CustomMailboxPermissions.ps1
@@ -15,7 +15,7 @@ function Remove-CustomMailboxPermissions {
         $MailboxPermissions = Get-MailboxPermission -Identity $Mailbox
         foreach ($MailboxPermission in $MailboxPermissions) {
             if ($MailboxPermission.User -notlike "*NT AUTHORITY\*") {
-            Remove-MailboxPermission -Identity $MailboxPermission.Identity -User $MailboxPermission.User -AccessRights $MailboxPermission.AccessRights
+            Remove-MailboxPermission -Identity $MailboxPermission.Identity -User $MailboxPermission.User -AccessRights $MailboxPermission.AccessRights -Confirm:$true
             }
         }
     }
@@ -25,32 +25,55 @@ function Remove-CustomMailboxPermissions {
         $RecipientPermissions = Get-RecipientPermission -Identity $Mailbox
         foreach ($RecipientPermission in $RecipientPermissions) {
             if ($RecipientPermission.Trustee -notlike "*NT AUTHORITY\*") {
-            Remove-RecipientPermission -Identity $RecipientPermission.Identity -Trustee $RecipientPermission.Trustee -AccessRights $RecipientPermission.AccessRights
+            Remove-RecipientPermission -Identity $RecipientPermission.Identity -Trustee $RecipientPermission.Trustee -AccessRights $RecipientPermission.AccessRights -Confirm:$true
             }
         }
     }
 
-    #Remove Mailbox Permissions and Recipient Permissions
-    if ($null -ne $Users) {
+    #Remove Mailbox Permissions and Recipient Permissions From All Mailboxes
+    if (($null -ne $Users) -and ($null -eq $Mailbox)) {
         $Mailboxes = Get-Mailbox
         foreach ($Mailbox in $Mailboxes) {
             foreach ($User in $Users) {
                 $MailboxPermissions = Get-MailboxPermission -Identity $Mailbox.Name -User $User.UserPrincipalName
                 if ($null -ne $MailboxPermissions) {
-                    Remove-MailboxPermission -Identity $Mailbox.Name -User $User.UserPrincipalName -AccessRights $MailboxPermissions.AccessRights -Confirm:$Confirm
+                    Remove-MailboxPermission -Identity $Mailbox.Name -User $User.UserPrincipalName -AccessRights $MailboxPermissions.AccessRights -Confirm:$true
                     }
             }
             foreach ($User in $Users) {
                 $RecipientPermissions = Get-RecipientPermission -Identity $Mailbox.Name -Trustee $User.UserPrincipalName
                 if ($null -ne $RecipientPermissions) {
-                    Remove-RecipientPermission -Identity $Mailbox.Name -Trustee $User.UserPrincipalName -AccessRights $RecipientPermissions.AccessRights -Confirm:$Confirm
+                    Remove-RecipientPermission -Identity $Mailbox.Name -Trustee $User.UserPrincipalName -AccessRights $RecipientPermissions.AccessRights -Confirm:$true
+                }
+            }
+        }
+    }
+
+    #Remove Mailbox Permissions and Recipient Permissions From Specified Mailbox
+    if (($null -ne $Users) -and ($null -ne $Mailbox)) {
+        $Mailboxes = Get-Mailbox $Mailbox
+        foreach ($Mailbox in $Mailboxes) {
+            foreach ($User in $Users) {
+                $MailboxPermissions = Get-MailboxPermission -Identity $Mailbox.Name -User $User.UserPrincipalName
+                if ($null -ne $MailboxPermissions) {
+                    Remove-MailboxPermission -Identity $Mailbox.Name -User $User.UserPrincipalName -AccessRights $MailboxPermissions.AccessRights -Confirm:$true
+                }
+            }
+            foreach ($User in $Users) {
+                $RecipientPermissions = Get-RecipientPermission -Identity $Mailbox.Name -Trustee $User.UserPrincipalName
+                if ($null -ne $RecipientPermissions) {
+                    Remove-RecipientPermission -Identity $Mailbox.Name -Trustee $User.UserPrincipalName -AccessRights $RecipientPermissions.AccessRights -Confirm:$true
                 }
             }
         }
     }
 }
 
+
 #Examples:
-Remove-CustomMailboxPermissions -Mailbox "OneTribe - DevOps"
-Remove-CustomMailboxPermissions -Mailbox "OneTribe - DevOps" -GroupDisplayName "OneTribe - DevOps"
-Remove-CustomMailboxPermissions -Mailbox "OneTribe - DevOps" -UserDisplayName "Peter Mijnster"
+Remove-CustomMailboxPermissions -Mailbox "OneTribe - DevOps"                                       #remove everyone from mailbox
+Remove-CustomMailboxPermissions -Mailbox "OneTribe - DevOps" -GroupDisplayName "OneTribe - DevOps" #remove everyone in group from mailbox
+Remove-CustomMailboxPermissions -Mailbox "OneTribe - DevOps" -UserDisplayName "Peter Mijnster"     #remove user from mailbox
+
+Remove-CustomMailboxPermissions -GroupDisplayName "OneTribe - DevOps"                              #remove everyone in group from all mailboxes
+Remove-CustomMailboxPermissions -UserDisplayName "Peter Mijnster"                                  #remove user from all mailboxes
